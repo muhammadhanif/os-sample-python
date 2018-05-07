573df5e566565ae36f176891567525e277646662
hanif <hanif@btech.co.id>
2018-05-07 20:41:20 +0700

    Update wsgi.py & index.html

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "Hello World!"
+    name = "nolsatu.id"
+    link = "https://www.nolsatu.id/"
+    return render_template("index.html", name=name, link=link)
 
 if __name__ == "__main__":
     application.run()
